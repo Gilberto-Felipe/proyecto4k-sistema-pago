c8be1326fb4ced004ddb978e5976089d43d39c7c
Gilberto-Felipe <gramirez25@ucol.mx>
2019-06-04 15:27:27 -0500

    Copio archivos que me servirán para este proyecto

---
+++ vistas/js/usuarios.js
@@ -0,0 +1,51 @@
+/*=============================================
+=          SUBIR FOTO DE USUARIO            =
+=============================================*/
+
+$(".nuevaFoto").change(function(){
+
+	let imagen = this.files[0];
+	console.log("imagen", imagen);
+
+	/*=============================================
+	=          VALIDAR FORMATO PNG O JPG           =
+	=============================================*/
+
+	if (imagen["type"] != "image/jpeg"  && imagen["type"] != "image/png") {
+
+		$(".nuevaFoto").val("");
+
+		swal({
+			title: "Error al subir la imagen",
+			text: "¡La imagen debe estar en formato .jpg o .png!",
+			type: "error",
+			confirmButtonText: "¡Cerrar!"
+		});
+
+	} else if (imagen["size"] > 2000000 ) {
+
+		$(".nuevaFoto").val("");
+
+		swal({
+			title: "Error al subir la imagen",
+			text: "¡La imagen debe pesar menos de 2MB!",
+			type: "error",
+			confirmButtonText: "¡Cerrar!"
+		});
+
+	}else {
+
+		let datosImagen = new FileReader();
+		datosImagen.readAsDataURL(imagen);
+
+		$(datosImagen).on("load", function(event) {
+
+			let rutaImagen = event.target.result;
+
+			$(".previsualizar").attr("src", rutaImagen);
+
+		});
+
+	}
+
+})
---
+++ vistas/modulos/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer" align="center">
+	
+	<strong>Copyright &copy; 2019 <a href="https://www.tutorialesatualcance.com" target="_blank">Tutoriales a tu alcance</a> </strong>
+	Todos los derechos reservados.
+
+</footer>
