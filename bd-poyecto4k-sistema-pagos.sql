7ba31d5e4006ad1c278ddc94ae2e81cbf5a6a684
Gilberto-Felipe <gramirez25@ucol.mx>
2019-06-12 12:11:46 -0500

    limpiando el proyecto

--- bd-poyecto4k-sistema-pagos.sql
+++ bd-poyecto4k-sistema-pagos.sql
@@ -1,4 +1,8 @@
-## Proyecto4k-sistema-pagos
+## 				Proyecto4k-sistema-pagos
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++ 						DDL														  -
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 drop database if exists proyecto4kPagos; 
 create database proyecto4kPagos CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -10,31 +14,45 @@ create table perfil(
     perfil varchar(15) not null,
     primary key (id)
 );
-desc perfil;
+-- desc perfil;
 
-drop table if exists usuario;
-create table usuario(
+drop table if exists concepto;
+create table concepto(
+	id int auto_increment,
+    concepto varchar(100) not null,
+    precio decimal(15,2) not null,
+    primary key (id)
+);
+-- desc concepto;
+
+drop table if exists usuarios;
+create table usuarios(
 	id int auto_increment,
     nombre varchar(50) not null,
     apellido varchar(50) not null,
     telefono varchar(10) not null,
     email varchar(255) not null,
-    contraseña varchar(8) not null, 
-	primary key (id)
+    contraseña varchar(10) not null, 
+    id_perfil int not null,
+	primary key (id),
+    constraint FK_perfilUsuario foreign key (id_perfil) references perfil(id)
 );
-desc usuario;
+-- desc usuario;
 
 drop table if exists deuda;
 create table deuda(
 	id int auto_increment,
     id_usuario int not null,
-    porcionDeuda decimal(15,2) not null,
-    fechaDeuda datetime not null,
-    estado varchar(10) not null,
+    id_concepto int not null,
+    cantidadDeuda decimal(15,2) not null,
+    fechaDeuda datetime,
+    deudaRestante decimal(15,2),
+    estadoDeuda bool not null,
     primary key (id), 
-    constraint FK_usuarioDeuda foreign key (id_usuario) references usuario(id)
+    constraint FK_usuarioDeuda foreign key (id_usuario) references usuario(id),
+    constraint FK_conceptoDeuda foreign key (id_concepto) references concepto(id)
 );
-desc deuda;
+-- desc deuda;
 
 drop table if exists detallePagos;
 create table detallePagos(
@@ -46,7 +64,59 @@ create table detallePagos(
     constraint FK_usuarioDetalles foreign key (id_usuario) references usuario(id),
     constraint FK_deudaDetalles foreign key (id_deuda) references deuda(id)
 );
-desc detallePagos;
+-- desc detallePagos;
 
-# fin
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
++ 							DML
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+# insertando datos a tabla perfil
+-- desc perfil;
+insert into perfil (perfil) values ('administrador');
+insert into perfil (perfil) values ('usuario');
+-- select * from perfil;
+
+# insertando datos en usuario
+-- desc usuario;
+insert into usuario (nombre, apellido, telefono, email, contraseña, id_perfil) 
+values ('Humberto', 'Ramírez', '1234554321', 'raghum76@ucol.mx', 'qweRty789', 1);
+insert into usuario (nombre, apellido, telefono, email, contraseña, id_perfil) 
+values ('Pedro', 'Ortiz', '2345678901', 'raghum76@ucol.mx', '123', 2);
+insert into usuario (nombre, apellido, telefono, email, contraseña, id_perfil) 
+values ('Gilberto', 'Ramírez', '3345678901', 'gramirez25@ucol.mx', '123', 2);
+insert into usuario (nombre, apellido, telefono, email, contraseña, id_perfil) 
+values ('Ismael', 'Meza', '4345678901', 'imeza@ucol.mx', '123', 2);
+insert into usuario (nombre, apellido, telefono, email, contraseña, id_perfil) 
+values ('Gerardo', 'Barreto', '5345678901', 'gbarreto@ucol.mx', '123', 2);
+-- select * from usuario;
+
+#insertando datos en concepto
+-- desc concepto;
+insert into concepto (concepto) values ('música');
+insert into concepto (concepto) values ('mesa');
+insert into concepto (concepto) values ('hora de mariachi');
+insert into concepto (concepto) values ('tequila');
+
+# insertando datos en deuda
+-- desc deuda;
+insert into deuda (id_usuario, id_concepto, cantidadDeuda, fechaDeuda, deudaRestante, estado) 
+values (2, 1, 500, '2019-06-07', null, 0);
+
+(id_usuario, id_concepto, cantidadDeuda, fechaDeuda, deudaRestante, estado) 
+values (2, 1, 500, '2019-06-07', null, 0);
+insert into deuda (id_usuario, id_concepto, porcionDeuda, fechaDeuda, estado) 
+values (4, 3, 1000, '2019-06-07',0);
+insert into deuda (id_usuario, porcionDeuda, fechaDeuda, estado) 
+values (5, 1, 500, '2019-06-07',1);
+-- select * from deuda;
+
+# insertando datos en detallePagos
+desc detallePagos;
+insert into detallePagos (id_usuario, id_deuda, cantidadPagada, fechaPago) 
+values (2, 1, 100, '2019-06-10');
+insert into detallePagos (id_usuario, id_deuda, cantidadPagada, fechaPago) 
+values (3, 1, 500, '2019-06-10');
+
 
--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -1,220 +1,30 @@
 <?php 
 
-class ControladorUsuarios {
+class ControladorUsuarios { 
 
-	/*=============================================
+    /*=============================================
 	LOGIN de USUARIOS              
-	=============================================*/
+    =============================================*/
 
-	static public function ctrIngresoUsuario(){
+    static public function ctrIngresoUsuarios() {
 
-		if (isset($_POST["ingUsuario"])) {
-			
-			if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"]) &&
-				preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])) {
+        if (preg) {
+            
+            if (preg_match('/^[0-9]+$/', $_POST["ingTelefono"]) &&
+				preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingContraseña"])) {
 
-				$encriptar = crypt($_POST["ingPassword"], '$2a$07$usesomesillystringforsalt$');
-				
-				$tabla = "usuarios";
+                    $tabla = "usuarios";
 
-				$item = "usuario";
+                    $item = "telefono";
+    
+                    $valor = $_POST["ingTelefono"];
+    
+                    $respuesta = ModeloUsuarios::mdlMostrarUsuarios($tabla, $item, $valor);
+            
+                }
 
-				$valor = $_POST["ingUsuario"];
+        }
 
-				$respuesta = ModeloUsuarios::mdlMostrarUsuarios($tabla, $item, $valor);
-
-				if ($respuesta["usuario"] == $_POST["ingUsuario"] && $respuesta["password"] == $encriptar){
-
-					$_SESSION['iniciarSesion'] = "ok";
-					$_SESSION['id'] = $respuesta['id'];
-					$_SESSION['nombre'] = $respuesta['nombre'];
-					$_SESSION['usuario'] = $respuesta['usuario'];
-					$_SESSION['foto'] = $respuesta['foto'];
-					$_SESSION['perfil'] = $respuesta['perfil'];
-
-					echo '<script>
-
-							window.location = "inicio";
-
-						 </script>';
-
-				}else{
-
-					echo '<br><div class="alert alert-danger">Error al ingresar. ¡Vuelve a intentarlo!</div>';
-
-				}
-
-			}
-
-		}
-
-	}
-
-	/*=============================================
-	CREACIÓN DE USUARIOS             =
-	=============================================*/
-
-	static public function ctrCrearUsuario(){
-
-		if (isset($_POST['nuevoUsuario'])) {
-			
-			if (preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚüÜ ]+$/', $_POST['nuevoNombre']) && 
-				preg_match('/^[a-zA-Z0-9]+$/', $_POST['nuevoUsuario']) && 
-				preg_match('/^[a-zA-Z0-9]+$/', $_POST['nuevoPassword'])){
-
-				/*=============================================
-				VALIDAR IMAGEN USUARIO
-				=============================================*/
-
-				$ruta = "";			
-
-				if (isset($_FILES['nuevaFoto']['tmp_name'])) {
-
-					list($ancho, $alto) = getimagesize($_FILES['nuevaFoto']['tmp_name']);
-
-					$nuevoAncho = 500;
-					$nuevoAlto = 500;
-
-					/*=============================================
-					CREAR DIRECTORIO PARA GUARDAR LA FOTO
-					=============================================*/	
-
-					$directorio = "vistas/img/usuarios/".$_POST['nuevoUsuario'];
-
-					mkdir($directorio, 0755);
-
-					/*=============================================
-					VALIDAR TIPO DE IMAGEN APLICANDO FUNCIONES DE PHP
-					=============================================*/	
-
-					if ($_FILES['nuevaFoto']['type'] == 'image/jpeg') {
-						
-					/*=============================================
-					GUARDAR IMAGEN EN  DIRECTORIO
-					=============================================*/
-
-					$aleatorio = mt_rand(100, 999);
-
-					$ruta = "vistas/img/usuarios/".$_POST['nuevoUsuario']."/".$aleatorio.".jpg";
-
-					$origen = imagecreatefromjpeg($_FILES['nuevaFoto']['tmp_name']);
-
-					$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
-
-					imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
-
-					imagejpeg($destino, $ruta);
-
-					}
-
-					if ($_FILES['nuevaFoto']['type'] == 'image/png') {
-						
-					/*=============================================
-					GUARDAR IMAGEN EN  DIRECTORIO
-					=============================================*/
-
-					$aleatorio = mt_rand(100, 999);
-
-					$ruta = "vistas/img/usuarios/".$_POST['nuevoUsuario']."/".$aleatorio.".png";
-
-					$origen = imagecreatefrompng($_FILES['nuevaFoto']['tmp_name']);
-
-					$destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
-
-					imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
-
-					imagepng($destino, $ruta);
-
-					}
-
-				}
-
-				/*=============================================
-				ENCRIPTAR CONTRASEÑA Y ENVIAR DATOS AL MODELO
-				=============================================*/
-
-				$encriptar = crypt($_POST['nuevoPassword'], '$2a$07$usesomesillystringforsalt$');
-
-				$tabla = "usuarios";
-
-				$datos = array(
-					"nombre" => $_POST['nuevoNombre'],
-					"usuario" => $_POST['nuevoUsuario'],
-					"password" => $encriptar,
-					"perfil" => $_POST['nuevoPerfil'],
-					"foto" => $ruta
-				);
-
-				$respuesta = ModeloUsuarios::mdlIngresarUsuario($tabla, $datos);
-
-				if ($respuesta == "ok") {
-					
-					echo '<script>
-
-					swal({
-
-						type: "success",
-						title: "¡El usuario se ha guardado correctamente!",
-						showConfirmButton: true,
-						confirmButtonText: "Cerrar",
-						closeOnConfirm: false						
-				
-					}).then((result)=>{
-
-						if(result.value){
-
-							window.location = "usuarios";
-
-						}
-
-					});
-
-					</script>';
-
-				}
-				
-			}else{
-
-				echo '<script>
-
-					swal({
-
-						type: "error",
-						title: "¡El usuario no puede ir vacío o llevar caracteres especiales!",
-						showConfirmButton: true,
-						confirmButtonText: "Cerrar",
-						closeOnConfirm: false						
-				
-					}).then((result)=>{
-
-						if(result.value){
-
-							window.location = "usuarios";
-
-						}
-
-					});
-
-					</script>';
-
-			}
-
-		}
-
-	}
-
-	/*=============================================
-	MOSTRAR USUARIOS              
-	=============================================*/
-
-	static public function ctrMostrarUsuarios($item, $valor){
-
-		$tabla = 'usuarios';
-
-		$respuesta = ModeloUsuarios::mdlMostrarUsuarios($tabla, $item, $valor);
-
-		return $respuesta;
-
-	}
+    }
 
 }
