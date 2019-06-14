7c644311c5aa49688e6a466fdcab8c4d049c6717
mauricioppgo <47857286+mauricioppgo@users.noreply.github.com>
2019-06-13 22:38:37 -0500

    vistas finales

---
+++ proyecto4k-sistema-pago-master/controladores/usuarios.controlador.php
@@ -0,0 +1,30 @@
+<?php 
+
+class ControladorUsuarios { 
+
+    /*=============================================
+	LOGIN de USUARIOS              
+    =============================================*/
+
+    static public function ctrIngresoUsuarios() {
+
+        if (preg) {
+            
+            if (preg_match('/^[0-9]+$/', $_POST["ingTelefono"]) &&
+				preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingContraseña"])) {
+
+                    $tabla = "usuarios";
+
+                    $item = "telefono";
+    
+                    $valor = $_POST["ingTelefono"];
+    
+                    $respuesta = ModeloUsuarios::mdlMostrarUsuarios($tabla, $item, $valor);
+            
+                }
+
+        }
+
+    }
+
+}
---
+++ proyecto4k-sistema-pago-master/progra/cobros.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:th="http://www.thymeleaf.org">
+
+<head>
+    <title>Mi primer Login</title>
+
+    <!--JQUERY-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <!-- FRAMEWORK BOOTSTRAP para el estilo de la pagina-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <!-- Los iconos tipo Solid de Fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+    <!-- Nuestro css-->
+    <link rel="stylesheet" type="text/css" href="../progra/index.css" th:href="@{/css/index.css}">
+
+</head>
+
+<body>
+    <div class="modal-dialog text-center">
+        <div class="col-sm-8 main-section">
+            <div class="modal-content">
+                <form class="col-12" th:action="@{/login}" method="get">
+                    
+                     <label for="psw-repeat"><b style="color: white">Monto</b></label>
+                    <input type="text" placeholder="Ingrese cantidad" required style="padding: 5px">
+                    
+                    <label for="psw-repeat"><b style="color: white">Deudor</b></label>
+                    <input type="number" placeholder="id del deudor" required style="padding: 5px">
+                    
+                    <label for="psw-repeat"><b style="color: white">Deuda</b></label>
+                    <input type="number" placeholder="id del deudor" required style="padding: 5px">
+                    
+                    <button type="submit" class="btn btn-primary" href="#"><i class="fas fa-sign-in-alt"></i>  Buscar </button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
